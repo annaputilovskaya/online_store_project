8c915d7b61ca2c0b5c40db99c7057a85079d4412
putilovskaya <putilovskayaa@mail.ru>
2024-04-18 22:13:22 +0300

    fix models.py

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {"blank": True, "null": True}
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание', **NULLABLE)
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание', **NULLABLE)
+    image = models.ImageField(upload_to='catalog/', verbose_name='Изображение', **NULLABLE)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, related_name='products', **NULLABLE)
+    price = models.IntegerField(verbose_name='Цена')
+    created_at = models.DateTimeField(auto_now=False, verbose_name='Дата создания')
+    updated_at = models.DateTimeField(auto_now_add=False, verbose_name='Дата последнего изменения')
+    manufactured_at = models.DateTimeField(verbose_name='Дата производства продукта')
+
+    def __str__(self):
+        return f'{self.name} ({self.category})'
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+
