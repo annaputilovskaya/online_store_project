3558c30fde80ed5cbc24083749e50d7bbdc5398a
putilovskaya <putilovskayaa@mail.ru>
2024-05-11 20:39:24 +0300

    modify urls

--- blog/urls.py
+++ blog/urls.py
@@ -8,8 +8,8 @@ app_name = BlogConfig.name
 
 urlpatterns = (
     path('', PostListView.as_view(), name='post_list'),
-    path('view/<int:pk>/', PostDetailView.as_view(), name='view'),
+    path('view/<slug:slug>/', PostDetailView.as_view(), name='view'),
     path('blog/create/', PostCreateView.as_view(), name='create'),
-    path('blog/<int:pk>/edit/', PostUpdateView.as_view(), name='edit'),
-    path('blog/<int:pk>/delete/', PostDeleteView.as_view(), name='delete'),
+    path('blog/<slug:slug>/edit/', PostUpdateView.as_view(), name='edit'),
+    path('blog/<slug:slug>/delete/', PostDeleteView.as_view(), name='delete'),
 )
