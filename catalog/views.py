5066611f2f0aad87a317534bcf4515cafba6a597
putilovskaya <putilovskayaa@mail.ru>
2024-05-18 21:20:14 +0300

    fix catalog/views.py

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,7 @@
-from django.views.generic import ListView, DetailView, TemplateView
+from django.urls import reverse_lazy, reverse
+from django.views.generic import ListView, DetailView, TemplateView, CreateView, UpdateView, DeleteView
 
+from catalog.forms import ProductForm
 from catalog.models import Product, Contacts
 
 
@@ -12,6 +14,25 @@ class ProductListView(ListView):
 
 class ProductDetailView(DetailView):
     model = Product
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    form_class = ProductForm
+    success_url = reverse_lazy('catalog:home')
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    form_class = ProductForm
+
+    def get_success_url(self):
+        return reverse('catalog:product_detail', args=[self.kwargs.get('pk')])
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    success_url = reverse_lazy('catalog:home')
 
 
 class ContactsPage(TemplateView):
